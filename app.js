ff000ad8e2e82f8a462da6ccb961f3ba9768f20e
XavierOrasmy <113276580+XavierOrasmy@users.noreply.github.com>
2023-12-16 21:22:34 -0500

    Update app.js
    Adds event for scrolling

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+let text = document.getElementById("title")
 
+      window.addEventListener("scroll",function(){
+        let value = window.scrollY;
+        text.style.marginBottom = value * 10 + "px"
+      })
